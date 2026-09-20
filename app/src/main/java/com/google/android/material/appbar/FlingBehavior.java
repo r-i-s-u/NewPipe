21f5fdb6212564b3f24f38505a7ab2072b7f414c
r-i-s-u <riishumaurya@gmail.com>
2026-09-20 06:39:28 +0530

    Remove seekbar preview, fix FlingBehavior

--- app/src/main/java/com/google/android/material/appbar/FlingBehavior.java
+++ app/src/main/java/com/google/android/material/appbar/FlingBehavior.java
@@ -27,8 +27,7 @@ public final class FlingBehavior extends AppBarLayout.Behavior {
     private boolean allowScroll = true;
     private final Rect globalRect = new Rect();
     private final List<Integer> skipInterceptionOfElements = List.of(
-            R.id.itemsListPanel, R.id.playbackSeekBar,
-            R.id.playPauseButton, R.id.playPreviousButton, R.id.playNextButton);
+);
 
     @Override
     public boolean onRequestChildRectangleOnScreen(
