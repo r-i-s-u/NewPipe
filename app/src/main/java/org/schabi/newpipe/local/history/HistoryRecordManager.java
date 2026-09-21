bce5f7e20643e2062bf4dd34149f46a24a56b2a7
r-i-s-u <riishumaurya@gmail.com>
2026-09-21 11:34:23 +0530

    Fix HistoryRecordManager import

--- app/src/main/java/org/schabi/newpipe/local/history/HistoryRecordManager.java
+++ app/src/main/java/org/schabi/newpipe/local/history/HistoryRecordManager.java
@@ -43,7 +43,6 @@ import org.schabi.newpipe.database.stream.model.StreamStateEntity;
 import org.schabi.newpipe.extractor.InfoItem;
 import org.schabi.newpipe.extractor.stream.StreamInfo;
 import org.schabi.newpipe.extractor.stream.StreamInfoItem;
-import org.schabi.newpipe.local.feed.FeedViewModel;
 import org.schabi.newpipe.player.playqueue.PlayQueueItem;
 import org.schabi.newpipe.util.ExtractorHelper;
 
